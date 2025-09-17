029e03ddab652d6d53cdcea6cdf7f52a6a520b52
Mell Ronald <ronalsgatsi@gmail.com>
2025-09-17 14:56:59 +0100

    feat: Add CourseSeeder

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Course;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $teachers = User::where('role', 'teacher')->get();
+
+        //pour chaque teacher on cree entre 1 a 4 cours
+        foreach ($teachers as $teacher) {
+            Course::factory(rand(1, 4))->create([
+                'user_id' => $teacher->id,
+            ]);
+        }
+    }
+}
