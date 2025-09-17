ac774120e0841171f7ee0a3c846900cd3294428a
Mell Ronald <ronalsgatsi@gmail.com>
2025-09-17 11:15:17 +0100

    feat Add Factory ProgressFactory nrollmentFactory

--- database/factories/EnrollmentFactory.php
+++ database/factories/EnrollmentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Course;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,9 @@ class EnrollmentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'course_id' => Course::factory(),
+            'enrolled_at' => fake()->dateTimeBetween('-1 years', 'now')
         ];
     }
 }
--- database/factories/ProgressFactory.php
+++ database/factories/ProgressFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Lesson;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,14 @@ class ProgressFactory extends Factory
      */
     public function definition(): array
     {
+        $is_completed = fake()->boolean(50);
+
         return [
-            //
+            'user_id' => User::factory(),
+            'lesson_id' => Lesson::factory(),
+            'time_spent' => $is_completed ? rand(45, 200) : null,
+            'is_completed' => $is_completed,
+            'last_accessed' => $is_completed ? fake()->dateTimeBetween('-6 months', 'now') : null,
         ];
     }
 }
