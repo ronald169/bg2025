f9754145408bcf4f949665479eea218f5438fbf8
Mell Ronald <ronalsgatsi@gmail.com>
2025-09-17 10:57:57 +0100

    feat: Factory LessonFactory

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,19 @@ class LessonFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'course_id' => Course::factory(),
+            'title' => fake()->sentence(4),
+            'content' => fake()->paragraphs(9, true),
+            'position' => 0,
+            'video_url' => null,
+            'duration' => fake()->randomElement([45, 60, 90, 120]),
         ];
+    }
+
+    public function is_free()
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_free' => true,
+        ]);
     }
 }
