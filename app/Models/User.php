c6a1f682347cd49787c032eaf4f69f82faec381d
Mell Ronald <ronalsgatsi@gmail.com>
2025-09-19 01:31:30 +0100

    feat: Add relation in User

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -21,6 +23,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'phone',
+        'data_of_birth',
+        'level',
+        'bio',
+        'profile_photo',
     ];
 
     /**
@@ -43,6 +51,55 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'date_of_birth' => 'date',
         ];
+    }
+
+    // Relation
+
+    // un Teacher peut creer plusieur Course
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    // LISTER les cours ou student est inscrit
+    public function coursesEnrolled(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'enrollments')
+                    ->withPivot('completed_at')
+                    ->withTimestamps();
+    }
+
+    // MANIPULER les inscriptions
+    public function enrollments() {
+        return $this->hasMany(Enrollment::class);
+    }
+
+    // Un student a plusieur progressions de lesson
+    public function progress(): HasMany
+    {
+        return $this->hasMany(Progress::class);
+    }
+
+    // Un student a plusieur tentative de quiz
+    public function quizAttempts(): HasMany
+    {
+        return $this->hasMany(QuizAttempt::class);
+    }
+
+    public function isTeacher()
+    {
+        return $this->role === 'teacher';
+    }
+
+    public function isStudent()
+    {
+        return $this->role === 'student';
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
     }
 }
