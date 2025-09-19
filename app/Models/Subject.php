478655c484f793269631f76b50f8efce7496182a
Mell Ronald <ronalsgatsi@gmail.com>
2025-09-19 01:48:12 +0100

    feat: Add Relation and accessor of Subject

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -4,9 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subject extends Model
 {
     /** @use HasFactory<\Database\Factories\SubjectFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'level',
+        'color',
+        'icon'
+    ];
+
+    // Relations
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    // Accessors
+    public function getDisplayNameAttribute()
+    {
+        return $this->name . ' (' . ucfirst($this->level) . ')';
+    }
 }
