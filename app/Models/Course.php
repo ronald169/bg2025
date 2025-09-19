bf5624f0ff9329cda80dd28bb32ff83fef9925c4
Mell Ronald <ronalsgatsi@gmail.com>
2025-09-19 02:10:37 +0100

    feat: Add Relation Accessors for Course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,82 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'objectives',
+        'prerequisites',
+        'price',
+        'image',
+        'difficulty',
+        'estimated_duration',
+        'is_published',
+        'user_id',
+        'subject_id',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+        'price' => 'decimal:2',
+        'estimated_duration' => 'integer'
+    ];
+
+    // Relation
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class)->orderBy('position');
+    }
+
+    public function enrollments(): HasMany
+    {
+        return $this->hasMany(Enrollment::class);
+    }
+
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'enrollments')
+                    ->withPivot('enrolled_at')
+                    ->withTimestamps();
+    }
+
+    // Accessors
+    public function getTotalLessonsAttribute()
+    {
+        return $this->lessons()->count();
+    }
+
+    public function getTotalDurationAttribute()
+    {
+        return $this->lessons()->sum('duration');
+    }
+
+    public function getEnrollmentCountAttribute()
+    {
+        return $this->enrollments()->count();
+    }
+
+    public function getAverageRatingAttribute()
+    {
+        return 4.5;
+    }
 }
