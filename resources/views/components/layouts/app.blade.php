e5e92776d1f374c440bc7f42d94e8c94c33df8f1
mellTrust <ronalsgatsi@gmail.com>
2026-07-13 23:06:59 +0100

    fix: quiz short_answer

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <link rel="stylesheet" href="{{ assert('/build/assets/app-D4GsFlKT.css') }}">
     <script src="{{ assert('/build/assets/app-Bj43h_rG.js') }}"></script>
     @livewireStyles
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -120,9 +120,9 @@
         }
     </style>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
-    <link rel="stylesheet" href="{{ asset('build/assets/app-QF-PWt_A.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-QF-PWt_A.css') }}"> --}}
     <script src="{{ asset('build/assets/app-CcNNqum8.js') }}"></script>
 
     <link rel="stylesheet" href="{{ asset('tinymce-custom.css') }}">
